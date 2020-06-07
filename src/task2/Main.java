8c2811871b37fa291d2173edd50edf120a877a60
Rihards Tīrums <r.tirums@gmail.com>
2020-06-07 18:19:30 +0300

    Adding more Clases to Project

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -12,14 +12,35 @@ public class Main {
         System.out.print("Please enter starting number from witch to print out: ");
         int y = scanner.nextInt();
         forLoop(x, y);
+        whileLoop(x, y);
+        doWhileLoop(x, y);
     }
 
     private static void forLoop(int x, int y){
         System.out.println("For Loop");
-        for (int i = 0; i < x ; i--) {
-            System.out.println(y++);
+        for (int i = 0; i < x ; i++) {
+            System.out.println(y--);
 
         }
+    }
+
+    private static void whileLoop(int x, int y){
+        System.out.println("While Loop");
+        int counter = 0;
+        while (counter < x){
+            System.out.println(y--);
+            counter++;
+        }
+
+    }
+
+    private static void doWhileLoop(int x, int y){
+        System.out.println("Do-While Loop");
+        int counter = 0;
+        do {
+            System.out.println(y--);
+            counter++;
+        }while (counter < x);
     }
 
 
---
+++ src/task3/Main.java
@@ -0,0 +1,26 @@
+package task3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        // Create Scanner for user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter amount of numbers you would like to print out: ");
+        int x = scanner.nextInt();
+        System.out.print("Please enter starting number from witch to print out: ");
+        int y = scanner.nextInt();
+
+        forLoop(x, y);
+    }
+
+    private static void forLoop(int x, int y){
+        System.out.println("For Loop");
+        for (int i = 0; i < ; i++) {
+
+        }
+
+    }
+}
